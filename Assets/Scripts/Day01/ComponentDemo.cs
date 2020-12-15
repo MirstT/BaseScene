e046b494148b938d3c55abf7650617d5caeb9020
MirstT <W_H_Norway@outlook.com>
2020-12-15 21:45:57 +0800

    ok改变位置与颜色

--- Assets/Scripts/Day01/ComponentDemo.cs
+++ Assets/Scripts/Day01/ComponentDemo.cs
@@ -9,6 +9,9 @@ public class ComponentDemo : MonoBehaviour
         if (GUILayout.Button("按钮"))
         {
             Debug.Log("ok");
+            //子类，继承父类 Component的元素
+            this.transform.position = new Vector3(0, 0, 10);
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
         }
     }
 
