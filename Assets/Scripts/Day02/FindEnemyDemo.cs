7ad74268e99a3479ae66c63b4118533cfd4caaff
MirstT <W_H_Norway@outlook.com>
2020-12-16 21:03:20 +0800

    查找血量最低的敌人

---
+++ Assets/Scripts/Day02/FindEnemyDemo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FindEnemyDemo : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUILayout.Button("查找血量最低的敌人"))
+        {
+            Enemy[] allEnemy = Object.FindObjectsOfType<Enemy>();
+            Enemy min = FindEnemyByMinHP(allEnemy);
+            min.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    public Enemy FindEnemyByMinHP(Enemy[] allEnemy)
+    {
+        Enemy min = allEnemy[0];
+
+        for (int i = 0; i < allEnemy.Length; i++)
+        {
+            if (min.HP > allEnemy[i].HP)
+            {
+                min = allEnemy[i];
+            }
+        }
+        return min;
+    }
+}
