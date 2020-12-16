8fb69f54543766524ce0ac39572eb148ca440ebe
MirstT <W_H_Norway@outlook.com>
2020-12-16 19:37:29 +0800

    Transform类提供了 查找变换组件（父，根，子）,改变位置，角度，大小功能

--- Assets/Scripts/Day01/TransformDemo.cs
+++ Assets/Scripts/Day01/TransformDemo.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Transform类提供了 查找变换组件（父，根，子）,改变位置，角度，大小功能
+/// </summary>
 public class TransformDemo : MonoBehaviour
 {
+    public Transform tf;
+
     private void OnGUI()
     {
         if (GUILayout.Button("foreach--transform"))
@@ -58,6 +63,41 @@ public class TransformDemo : MonoBehaviour
                 //this.transform.Rotate(0, 10, 0);
                 //向世界坐标系y轴
                 this.transform.Rotate(0, 10, 0, Space.World);
+            }
+        }
+
+        if (GUILayout.Button("root/parent"))
+        {
+            //获取根物体变换组件
+            Transform rootTF = this.transform.root;
+            //获取父物体变换组件
+            Transform parentTF = this.transform.parent;
+        }
+
+        if (GUILayout.Button("set root/parent"))
+        {
+            this.transform.SetParent(tf);
+        }
+        if (GUILayout.Button("set root/parent notworld"))
+        {
+            this.transform.SetParent(tf, false);
+        }
+
+        if (GUILayout.Button("Find( / / / )"))
+        {
+            //根据名称获取子物体
+            Transform childTF = this.transform.Find("子物体名称/下一级/下一级");
+        }
+
+        Transform[] childTFArray = new Transform[100];
+
+        if (GUILayout.Button("by 索引"))
+        {
+            //还是只能子物体
+            int count = this.transform.childCount;
+            for (int i = 0; i < count; i++)
+            {
+                childTFArray[i] = this.transform.GetChild(i);
             }
         }
     }
