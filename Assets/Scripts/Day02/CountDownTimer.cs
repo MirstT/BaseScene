84c58062437df37664536d939e5e1032b199b20f
MirstT <W_H_Norway@outlook.com>
2020-12-16 23:16:24 +0800

    倒计时实现的几种方式

---
+++ Assets/Scripts/Day02/CountDownTimer.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 倒计时
+/// </summary>
+public class CountDownTimer : MonoBehaviour
+{
+    //1.查找组件引用
+    //2.定义变量：秒second
+    //3.时间转换
+    //4.修改文本
+    private Text txtTimer;
+
+    public int second = 120;
+
+    private void Start()
+    {
+        txtTimer = this.GetComponent<Text>();
+        //启动调用
+        InvokeRepeating("Timer03", 0, 1);
+    }
+
+    public float nextTime = 1;//下次修改时间
+
+    private void Update()
+    {
+        //Timer01();
+        //Timer02();
+    }
+
+    //1:Time.time
+    private void Timer01()
+    {
+        if (Time.time >= nextTime)
+        {
+            second--;
+            txtTimer.text = string.Format("{0:d2}:{1:d2}", second / 60, second % 60);
+            if (second <= 10)
+            {
+                txtTimer.color = Color.red;
+            }
+            //nextTime = Time.time + 1;
+            nextTime += 1;
+        }
+    }
+
+    //2:Time.deltaTime
+    private float totalTime;
+
+    private void Timer02()
+    {
+        totalTime += Time.deltaTime;
+        if (totalTime >= 1)
+        {
+            second--;
+            txtTimer.text = string.Format("{0:d2}:{1:d2}", second / 60, second % 60);
+            totalTime = 0;
+        }
+    }
+
+    //3:Invoke()
+    //每隔固定时间重复执行
+    private void Timer03()
+    {
+        second--;
+        txtTimer.text = string.Format("{0:d2}:{1:d2}", second / 60, second % 60);
+
+        if (second <= 0)
+        {
+            //取消调用
+            CancelInvoke("Timer03");
+        }
+    }
+
+    //5.如何每秒修改一次
+    //重点：在Update每帧执行的方法中，个别语句实现指定间隔执行一次.
+    //Time.time
+}
---
+++ Assets/Scripts/Day02/TimeDemo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDemo : MonoBehaviour
+{
+    public float time;
+    public float speed = 100;
+    public float deltaTimea, unscaledDeltaTimeb, realtimeSinceStartupc, unscaledTimed;
+
+    public void Update()
+    {
+        time = Time.time;//受缩放影响的游戏运行时间
+        deltaTimea = Time.deltaTime;
+        unscaledDeltaTimeb = Time.unscaledDeltaTime;//不受缩放影响的游戏运行时间
+        realtimeSinceStartupc = Time.realtimeSinceStartup;//游戏实际运行时间
+        unscaledTimed = Time.unscaledTime;
+        //this.transform.Rotate(0, speed * Time.deltaTime, 0);
+
+        //游戏暂停，个别物体不受影响  Time.unscaledDeltaTime不受缩放影响的每帧间隔
+        this.transform.Rotate(0, speed * Time.unscaledDeltaTime, 0);
+    }
+
+    //0.02s
+    public void FixedUpdate()
+    {
+        this.transform.Rotate(speed, 0, 0);
+    }
+
+    //游戏暂停
+    //timeScale仅仅对时间造成影响 FixedUpdate deltaTime 并不会对Update造成影响
+    private void OnGUI()
+    {
+        if (GUILayout.Button("暂停游戏"))
+        {
+            Time.timeScale = 0;
+        }
+        if (GUILayout.Button("继续游戏"))
+        {
+            Time.timeScale = 1;
+        }
+        if (GUILayout.Button("加速游戏"))
+        {
+            Time.timeScale = 3;
+        }
+    }
+}
