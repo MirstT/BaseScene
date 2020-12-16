ecd997f4807b0f22bbf77bec908204fb69628ac1
MirstT <W_H_Norway@outlook.com>
2020-12-16 18:03:15 +0800

    WEEK3-2 UGUI练习

---
+++ Assets/Scripts/Week3_2/Week3_2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Week3_2 : MonoBehaviour
+{
+    public GameObject[] obj;
+    private int counter = 0;
+    public Text text;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (counter % 2 == 0)
+            foreach (var item in obj)
+                item.SetActive(true);
+        else
+            foreach (var item in obj)
+                item.SetActive(false);
+
+    }
+
+    public void OnClick()
+    {
+        counter++;
+    }
+}
